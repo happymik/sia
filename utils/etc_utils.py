3a384069a0866e8189e9ff2d510b1c24187fbc53
TonySimonovsky <metton@gmail.com>
2024-11-30 14:08:38 +0800

    + logging and exceptions

--- utils/etc_utils.py
+++ utils/etc_utils.py
@@ -1,5 +1,11 @@
 import requests
 
+from utils.logging_utils import setup_logging, log_message, enable_logging
+logger = setup_logging()
+enable_logging(True)
+
+
+
 def save_image_from_url(image_url, save_path):
     response = requests.get(image_url)
     if response.status_code == 200:
@@ -14,5 +20,10 @@ def save_image_from_url(image_url, save_path):
 from langchain_community.utilities.dalle_image_generator import DallEAPIWrapper
 
 def generate_image_dalle(input_prompt):
-    image_url = DallEAPIWrapper().run(input_prompt)
-    return image_url
+    try:
+        image_url = DallEAPIWrapper().run(input_prompt)
+        log_message(logger, "info", generate_image_dalle, f"Generated image with DALL-E: {image_url}")
+        return image_url
+    except Exception as e:
+        log_message(logger, "error", generate_image_dalle, f"Error generating image with DALL-E: {e}")
+        return None
