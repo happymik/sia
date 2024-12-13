1633dd8c157dd4a54945aec683cab578cc32d8fc
TonySimonovsky <metton@gmail.com>
2024-12-13 15:46:51 +0800

    changing the way new responses are tracked

--- sia/clients/twitter/twitter_official_api_client.py
+++ sia/clients/twitter/twitter_official_api_client.py
@@ -13,6 +13,8 @@ from sia.memory.memory import SiaMemory
 from sia.character import SiaCharacter
 from utils.logging_utils import setup_logging, log_message, enable_logging
 
+from langchain.prompts import ChatPromptTemplate
+
 class SiaTwitterOfficial(SiaClient):
     
     def __init__(self, api_key, api_secret_key, access_token, access_token_secret, bearer_token, sia = None, character: SiaCharacter = None, memory: SiaMemory = None, logging_enabled=True):
@@ -20,7 +22,8 @@ class SiaTwitterOfficial(SiaClient):
             client=tweepy.Client(
                 consumer_key=api_key, consumer_secret=api_secret_key,
                 access_token=access_token, access_token_secret=access_token_secret,
-                bearer_token=bearer_token
+                bearer_token=bearer_token,
+                wait_on_rate_limit=True
             )
         )
 
@@ -89,106 +92,15 @@ class SiaTwitterOfficial(SiaClient):
             return max(replies, key=lambda reply: reply.id).id
 
 
-    # def get_new_replies_to_my_tweets(self) -> list[SiaMessageSchema]:
-    #     since_id = self.get_last_retrieved_reply_id()
-
-    #     # sorted to process newer tweets first
-    #     my_tweet_ids = sorted(self.get_my_tweet_ids(), key=int, reverse=True)
-
-    #     log_message(self.logger, "info", self, f"since_id: {since_id}")
-    #     log_message(self.logger, "info", self, f"my_tweet_ids: {my_tweet_ids}")
-
-    #     messages = []
-        
-    #     time_to_sleep_before_next_batch = 61
-
-    #     for i in range(0, len(my_tweet_ids), 10):
-    #         time.sleep(time_to_sleep_before_next_batch)
-    #         batch = my_tweet_ids[i:i+10]
-    #         query = " OR ".join([f"conversation_id:{id}" for id in batch])
-    #         log_message(self.logger, "info", self, f"query: {query}")
-
-    #         try:
-    #             new_replies_to_my_tweets = self.client.search_recent_tweets(
-    #                 query=query,
-    #                 since_id=since_id,
-    #                 tweet_fields=["conversation_id","created_at","in_reply_to_user_id"],
-    #                 expansions=["author_id","referenced_tweets.id"]
-    #             )
-    #         except Exception as e:
-    #             log_message(self.logger, "error", self, f"Error getting replies: {e}")
-    #             time_to_sleep_before_next_batch += 30
-    #             continue
-            
-    #         if not new_replies_to_my_tweets.data:
-    #             continue
-            
-    #         for reply in new_replies_to_my_tweets.data:
-                
-    #             # exclude replies from the character itself
-    #             author = next((user.username for user in new_replies_to_my_tweets.includes['users'] if user.id == reply.author_id), None)
-    #             log_message(self.logger, "info", self, f"author of the received reply: {author}")
-    #             if author == self.character.twitter_username:
-    #                 continue
-                
-    #             try:
-    #                 from openai import OpenAI
-    #                 client = OpenAI()
-    #                 moderation_response = client.moderations.create(
-    #                     model="omni-moderation-latest",
-    #                     input=reply.text,
-    #                 )
-    #                 flagged = moderation_response.results[0].flagged
-    #                 if flagged:
-    #                     log_message(self.logger, "info", self, f"flagged reply: {reply.text}")
-    #             except Exception as e:
-    #                 log_message(self.logger, "error", self, f"Error moderating reply: {e}")
-    #                 flagged = False
-
-    #             try:
-    #                 message = self.memory.add_message(
-    #                     SiaMessageGeneratedSchema(
-    #                         conversation_id=str(reply.data['conversation_id']),
-    #                         content=reply.text,
-    #                         platform="twitter",
-    #                         author=next(user.username for user in new_replies_to_my_tweets.includes['users'] if user.id == reply.author_id),
-    #                         response_to=str(next((ref.id for ref in reply.referenced_tweets if ref.type == "replied_to"), None)) if reply.referenced_tweets else None,
-    #                         wen_posted=reply.created_at,
-    #                         flagged=int(flagged),
-    #                         metadata=moderation_response
-    #                     ),
-    #                     tweet_id=str(reply.id),
-    #                     original_data=reply.data
-    #                 )
-    #                 messages.append(message)
-    #             except Exception as e:
-    #                 log_message(self.logger, "error", self, f"Error adding message: {e}")
-
-    #     return messages
-
-
     def get_new_replies_to_my_tweets(self) -> list[SiaMessageSchema]:
         since_id = self.get_last_retrieved_reply_id()
-
-        # sorted to process newer tweets first
-        my_tweet_ids = sorted(self.get_my_tweet_ids(), key=int, reverse=True)
-
         log_message(self.logger, "info", self, f"since_id: {since_id}")
-        # log_message(self.logger, "info", self, f"my_tweet_ids: {my_tweet_ids}")
 
         messages = []
         
-        # time_to_sleep_before_next_batch = 61
-
-        # for i in range(0, len(my_tweet_ids), 10):
-        #     time.sleep(time_to_sleep_before_next_batch)
-        #     batch = my_tweet_ids[i:i+10]
-        #     query = " OR ".join([f"conversation_id:{id}" for id in batch])
-        #     log_message(self.logger, "info", self, f"query: {query}")
-
         try:
             new_replies_to_my_tweets = self.client.search_recent_tweets(
-                query=f"to:{self.character.twitter_username}",
+                query=f"to:{self.character.twitter_username} OR @{self.character.twitter_username}",
                 since_id=since_id,
                 tweet_fields=["conversation_id","created_at","in_reply_to_user_id"],
                 expansions=["author_id","referenced_tweets.id"]
@@ -196,8 +108,6 @@ class SiaTwitterOfficial(SiaClient):
         except Exception as e:
             log_message(self.logger, "error", self, f"Error getting replies: {e}")
             return []
-            # time_to_sleep_before_next_batch += 30
-            # continue
         
         if not new_replies_to_my_tweets.data:
             return []
@@ -272,9 +182,15 @@ class SiaTwitterOfficial(SiaClient):
             next_post_minutes = (next_post_time_seconds % 3600) // 60
             print(f"Next post time: {next_post_datetime} (posting in {next_post_hours}h {next_post_minutes}m)")
             
+            
+
+
+            
             # posting
             #   new tweet
             if time.time() > next_post_time:
+                
+                
                 post, media = self.sia.generate_post(
                     platform="twitter",
                     author=self.character.twitter_username,
@@ -329,15 +245,16 @@ class SiaTwitterOfficial(SiaClient):
                             continue
                         print(f"Generated response: {len(generated_response.content)} characters")
                         tweet_id = self.sia.twitter.publish_post(post=generated_response, in_reply_to_tweet_id=r.id)
+                        self.memory.add_message(message_id=tweet_id, message=generated_response)
                         replies_sent += 1
                         if isinstance(tweet_id, Forbidden):
                             print(f"\n\nFailed to send reply: {tweet_id}. Sleeping for 10 minutes.\n\n")
                             time.sleep(600)
-                        time.sleep(random.randint(20, 40))
+                        time.sleep(random.randint(70, 90))
                 else:
                     print("No new replies yet.")
                 print("\n\n")
 
-            time.sleep(random.randint(20, 40))
+            time.sleep(random.randint(70, 90))
 
 
